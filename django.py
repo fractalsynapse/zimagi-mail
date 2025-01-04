f35d90ce2c3692073ab1d3036f08378d0840d61f
Adrian Webb <adrian@webb.sh>
2025-01-04 11:43:55 -0500

    Updating some default values in the Django settings.

--- django.py
+++ django.py
@@ -8,7 +8,7 @@ from settings.config import Config
 EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 ANYMAIL = {
     'MAILGUN_API_KEY': Config.string('ZIMAGI_MAILGUN_API_KEY'),
-    'MAILGUN_SENDER_DOMAIN': Config.string('ZIMAGI_MAILGUN_DOMAIN', 'nexical.email'),
+    'MAILGUN_SENDER_DOMAIN': Config.string('ZIMAGI_MAILGUN_DOMAIN', 'example.com'),
     'MAILGUN_API_URL': Config.string('ZIMAGI_MAILGUN_API_URL', 'https://api.mailgun.net/v3'),
 }
 
@@ -16,7 +16,7 @@ ANYMAIL = {
 # Email configurations
 #
 EMAIL_SITE_NAME = Config.string('ZIMAGI_EMAIL_SITE_NAME', 'Nexical Federal Contract Explorer')
-ROOT_EMAIL_URL = Config.string('ZIMAGI_ROOT_EMAIL_URL', 'https://contracts.nexical.ai')
+ROOT_EMAIL_URL = Config.string('ZIMAGI_ROOT_EMAIL_URL', 'https://example.com')
 
-DEFAULT_FROM_EMAIL = Config.string('ZIMAGI_DEFAULT_FROM_EMAIL', 'Nexical Contracts <noreply@nexical.email>')
+DEFAULT_FROM_EMAIL = Config.string('ZIMAGI_DEFAULT_FROM_EMAIL', 'No Reply <noreply@example.com>')
 EMAIL_SUBJECT_PREFIX = Config.string('ZIMAGI_EMAIL_SUBJECT_PREFIX', '')
